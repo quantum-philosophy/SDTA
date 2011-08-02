4cf0dae4107a61d1cece9ff2e4bfcb06c85c9ef4
Ivan Ukhov <uvsoft@gmail.com>
2011-08-03 00:06:24 +0200

    Fix calcError.m

--- src/+Utils/calcError.m
+++ src/+Utils/calcError.m
@@ -1,4 +1,5 @@
 function error = calcError(T1, T2)
+  error = zeros(size(T1, 1), size(T1, 2), 3);
   error(:, :, 1) = T1 - T2;
   error(:, :, 2) = [ T1(2:end, :); T1(1, :) ] - T2;
   error(:, :, 3) = [ T1(end, :); T1(1:end-1, :) ] - T2;
