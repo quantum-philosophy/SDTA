cfe5099bb9055e8859b1aba0ec448443024386d3
Ivan Ukhov <uvsoft@gmail.com>
2011-08-19 21:19:30 +0200

    Shrink the size of some test cases

--- src/Constants.m
+++ src/Constants.m
@@ -13,13 +13,13 @@ classdef Constants < handle
     % Visualization
     roundRobinColors = { 'r', 'g', 'b', 'm', 'y', 'c' };
 
-    % Sampling interval
-    samplingInterval = 1e-3; % s
-
     % Kelvin to Celsius
     degreeKelvin = 273.15; % K
 
     % Ambient temperature
     ambientTemperature = 27 + Constants.degreeKelvin; % K
+
+    % Sampling interval
+    samplingInterval = 1e-4; % s
   end
 end
