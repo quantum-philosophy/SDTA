500aa48aa3cf0962468ab53c87aab8ecfbf1af74
Ivan Ukhov <uvsoft@gmail.com>
2011-10-01 09:22:20 +0200

    Change a bit testRanking

--- msrc/testRanking.m
+++ msrc/testRanking.m
@@ -1,30 +1,30 @@
-factor = -0.1;
+factor = -0.25;
 
 population_size = 300;
 expectations = zeros(1, population_size);
 
-sum = 0;
+s = 0;
 
 for i = 1:population_size
   expectations(i) = i ^ factor;
-  sum = sum + expectations(i);
+  s = s + expectations(i);
 end
 
 total = 0;
 
 for i = 1:population_size
-  expectations(i) = population_size * expectations(i) / sum;
+  expectations(i) = population_size * expectations(i) / s;
   total = total + expectations(i);
 end
 
 plot(1:population_size, expectations);
 
-fprintf('%5s%15s%15s\n', 'Rank', 'Probability', 'Cumulative');
+fprintf('%5s%15s%15s%15s\n', 'Rank', 'Expectation', 'Probability', 'Cumulative');
 
 F = 0;
 
 for i = 1:population_size
   p = expectations(i) / total;
   F = F + p;
-  fprintf('%5d%15.4f%15.4f\n', i, p, F);
+  fprintf('%5d%15.4f%15.4f%15.4f\n', i, expectations(i), p, F);
 end
