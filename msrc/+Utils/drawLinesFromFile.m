25b5374f67f92e0070b521d2ecf2568801890dbc
Ivan Ukhov <uvsoft@gmail.com>
2011-10-06 08:53:07 +0200

    Some adjustments in the Matlab code

--- msrc/+Utils/drawLinesFromFile.m
+++ msrc/+Utils/drawLinesFromFile.m
@@ -1,10 +1,11 @@
-function drawLinesFromFile(file, name, limits)
+function drawLinesFromFile(file, name, limits, offset)
   if nargin < 2 || isempty(name), name = 'Temperature'; end
   if nargin < 3, limits = []; end
+  if nargin < 4, offset = 0; end
 
   figure;
 
-  matrix = dlmread(file);
+  matrix = dlmread(file) + offset;
   x = ((1:size(matrix, 1)) - 1) * Constants.samplingInterval;
   Utils.drawLines(name, 'Time, s', name, x, matrix);
 
--- msrc/testCE.m
+++ msrc/testCE.m
@@ -4,7 +4,7 @@ clear all;
 clc;
 rng(0);
 
-[ graph, hotspot, powerProfile ] = setup('002_030');
+[ graph, hotspot, powerProfile ] = setup('004_060');
 
 graph.inspect();
 
