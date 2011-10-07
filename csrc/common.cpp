a38e2a2aee6d61112f5eb6cc462034580a07fec6
Ivan Ukhov <uvsoft@gmail.com>
2011-10-07 14:13:02 +0200

    Fix string.h in common.h/cpp

--- csrc/common.cpp
+++ csrc/common.cpp
@@ -4,7 +4,6 @@
 #include <fstream>
 #include <string>
 #include <iomanip>
-#include <string.h>
 
 #include "common.h"
 
--- csrc/common.h
+++ csrc/common.h
@@ -13,6 +13,7 @@
 #include <cmath>
 #include <algorithm>
 #include <stdlib.h>
+#include <string.h>
 
 #include "Random.h"
 
