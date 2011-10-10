b4ba48c7a62546ac74803c009b10cab37927d904
Ivan Ukhov <uvsoft@gmail.com>
2011-10-10 18:37:33 +0200

    Construct band

--- msrc/@Hotspot/Hotspot.m
+++ msrc/@Hotspot/Hotspot.m
@@ -26,7 +26,7 @@ classdef Hotspot < handle
     end
 
     function [ T, time ] = solve(hs, power)
-      [ A, B ] = hs.constructSystem(power, hs.samplingInterval);
+      [ A, B ] = hs.constructBand(power, hs.samplingInterval);
 
       tic;
 
@@ -61,30 +61,67 @@ classdef Hotspot < handle
       G = hs.DV * diag((exp(t * hs.DL) - 1) ./ hs.DL) * hs.DVT * hs.sinvC;
     end
 
-    function [ A, B ] = constructSystem(hs, P, t)
-      nodes = hs.nodes;
-      [ steps, cores ] = size(P);
+    function [ A, B ] = constructFull(hs, P, t)
+      [ m, cores ] = size(P);
+      n = hs.nodes;
 
-      total = nodes * steps;
+      nm = n * m;
 
-      P = [ P, zeros(steps, nodes - cores) ];
+      P = [ P, zeros(m, n - cores) ];
 
-      A = zeros(total, total);
-      B = zeros(total, 1);
+      A = zeros(nm, nm);
+      B = zeros(nm, 1);
 
       [ expDt, G ] = hs.calculateCoefficients(t);
 
-      for i = 1:steps
-        j = (i - 1) * nodes + 1;
-        k = j + nodes;
-        o = nodes - 1;
-        if (k > total)
+      for i = 1:m
+        j = (i - 1) * n + 1;
+        k = j + n;
+        o = n - 1;
+        if (k > nm)
           k = 1;
         end
         A(j:(j + o), j:(j + o)) = expDt;
-        A(j:(j + o), k:(k + o)) = -eye(nodes);
+        A(j:(j + o), k:(k + o)) = -eye(n);
         B(j:(j + o), 1) = - G * transpose(P(i, :));
       end
+    end
+
+    function [ A, B ] = constructBand(hs, P, t)
+      [ m, cores ] = size(P);
+      n = hs.nodes;
+
+      nm = n * m;
+
+      P = [ P, zeros(m, n - cores) ];
+
+      B = zeros(nm, 1);
+
+      m1 = n;
+      m2 = n;
+      p = m1 + 1 + m2;
+      d = [ -(nm - n), -(n - 1):(n - 1), n ];
+      A = zeros(nm, p);
+
+      [ expDt, G ] = hs.calculateCoefficients(t);
+
+      for s = 1:m
+        o = 0;
+        for j = 1:n
+          r = (s - 1) * n + j;
+          for i = 1:n
+            A(r, m1 + 1 - (i - 1) + o) = expDt(i, j);
+          end
+          o = o + 1;
+        end
+        r = (s - 1) * n + 1;
+        B(r:(r + n - 1), 1) = - G * transpose(P(s, :));
+      end
+
+      A(:, 1) = -ones(nm, 1);
+      A(:, p) = -ones(nm, 1);
+
+      A = spdiags(A, d, nm, nm);
     end
   end
 end
