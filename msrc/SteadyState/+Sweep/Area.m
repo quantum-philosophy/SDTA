e761b1f9c4cd392ff7350cc97145671067a06a27
Ivan Ukhov <uvsoft@gmail.com>
2011-10-17 19:49:09 +0200

    Keep the heat sink and spreader ratio along with the die area

--- msrc/SteadyState/+Sweep/Area.m
+++ msrc/SteadyState/+Sweep/Area.m
@@ -1,4 +1,9 @@
 classdef Area < Sweep.Basic
+  properties (Constant)
+    keepRatio = true;
+    ratio = (37.5 * 37.5) / 81; % Heat sink area to die area
+  end
+
   properties (SetAccess = private)
     processorArea
     powerScale
@@ -8,7 +13,11 @@ classdef Area < Sweep.Basic
   methods
     function sweep = Area(test, processorArea)
       sweep = sweep@Sweep.Basic(test);
-      sweep.variable = 'Area of one core, mm^2';
+      if sweep.processorCount == 1
+        sweep.variable = 'Area of the die, mm^2';
+      else
+        sweep.variable = 'Area of one processing element, mm^2';
+      end
       sweep.floorplan = Utils.path([ test, '_tmp.flp' ]);
 
       sweep.processorArea = processorArea;
@@ -25,10 +34,26 @@ classdef Area < Sweep.Basic
     end
 
     function config = setupStep(sweep, i)
-      Utils.generateFloorplan(sweep.floorplan, ...
-        sweep.processorCount, sweep.processorArea(i));
+      area = sweep.processorArea(i);
 
-      config = { 'power_scale', sweep.powerScale };
+      Utils.generateFloorplan(sweep.floorplan, ...
+        sweep.processorCount, area);
+
+      if sweep.keepRatio
+        % Die
+        dieArea = area * sweep.processorCount;
+        dieSide = sqrt(dieArea);
+        % Sink
+        sinkSide = sqrt(sweep.ratio * dieArea);
+        % Spreader
+        spreaderSide = (dieSide + sinkSide) / 2;
+
+        hotspot_line = sprintf('s_sink %.4f s_spreader %.4f', sinkSide, spreaderSide);
+
+        config = { 'hotspot', hotspot_line, 'power_scale', sweep.powerScale };
+      else
+        config = { 'power_scale', sweep.powerScale };
+      end
     end
 
     function [ value, retake ] = valueStep(sweep, i, Tce, Tss, power)
--- msrc/SteadyState/compareCurves.m
+++ msrc/SteadyState/compareCurves.m
@@ -4,18 +4,35 @@ config = Optima('001_030');
 
 processorArea = 81e-6;
 powerScale = 10;
+time_scale = 1;
 
 config.changeArea(processorArea);
 
+ratio = (37.5 * 37.5) / 81;
+
+% Die
+dieArea = processorArea * config.processorCount;
+dieSide = sqrt(dieArea);
+% Sink
+sinkSide = sqrt(ratio * dieArea);
+% Spreader
+spreaderSide = (dieSide + sinkSide) / 2;
+
+hotspot_line = sprintf( ...
+  's_sink %.4f s_spreader %.4f', ...
+  sinkSide, spreaderSide);
+
 param_line = @(leakage, solution, power_scale) ...
   Utils.configStream(...
+    'hotspot', hotspot_line, ...
     'verbose', 0, ...
     'deadline_ratio', 1, ...
     'leakage', leakage, ...
     'solution', solution, ...
+    'time_scale', time_scale, ...
     'power_scale', power_scale);
 
-Tce = Optima.solve(config.system, config.floorplan, config.hotspot, config.params, ...
+[ Tce, power ] = Optima.solve(config.system, config.floorplan, config.hotspot, config.params, ...
   param_line(0, 'condensed_equation', powerScale));
 
 Tss = Optima.solve(config.system, config.floorplan, config.hotspot, config.params, ...
@@ -24,6 +41,9 @@ Tss = Optima.solve(config.system, config.floorplan, config.hotspot, config.param
 [ stepCount, processorCount ] = size(Tce);
 time = ((1:stepCount) - 1) * config.samplingInterval;
 
+Pmax = max(max(power));
+fprintf('Maximal power consumption: %.2f W\n', Pmax);
+
 Tce = Tce - Constants.degreeKelvin;
 Tss = Tss - Constants.degreeKelvin;
 
@@ -31,13 +51,17 @@ figure;
 
 subplot(2, 1, 1);
 Utils.compareLines('Steady-State Dynamic Temperature Curve', ...
-  'Time, s', 'Temperature, C', time, 'CE', Tce, 'SS', Tss);
+  'Time, s', 'Temperature, C', time, ...
+  'SSDTC', Tce, 'Steady-State Approximation', Tss);
 
 tgff = TestCase.TGFF(config.tgff);
 graph = tgff.graphs{1};
 pes = tgff.pes;
 LS.mapEarliestAndSchedule(graph, pes);
 graph.assignDeadline(graph.duration);
+graph.scale(time_scale);
 
 subplot(2, 1, 2);
 graph.draw(false);
+
+graph.inspect();
