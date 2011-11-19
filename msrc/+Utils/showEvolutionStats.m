2945234b0bb1b8c522b1e97da7c01946f7ff7f5f
Ivan Ukhov <uvsoft@gmail.com>
2011-11-19 15:28:09 +0100

    Improve showEvolutionStats

--- msrc/+Utils/showEvolutionStats.m
+++ msrc/+Utils/showEvolutionStats.m
@@ -8,17 +8,24 @@ function showEvolutionStats(file, full)
   prefix = prefix{1};
   prefix = prefix{1};
 
-  generations = [];
-  evaluations = [];
-  deadline_misses = [];
-  improvement = [];
-  assessed_improvement = [];
-  lifetime = [];
-  time = [];
+  G = 1;
+  E = 2;
+  D = 3;
+  R = 4;
+  IL = 5;
+  IE = 6;
+  AL = 7;
+  AE = 8;
+  T = 9;
+
+  Stats = zeros(0, 9);
 
   for i = 1:count
     file = sprintf('%s/%s', prefix, files(i).name);
     fid = fopen(file);
+
+    stats = zeros(0, size(Stats, 2));
+    count = 0;
 
     line = fgetl(fid);
     while ischar(line)
@@ -30,82 +37,72 @@ function showEvolutionStats(file, full)
 
         switch (name)
         case 'Generations'
-          generations(end + 1) = str2num(value);
+          count = count + 1;
+          stats(end + 1, :) = zeros(1, size(stats, 2));
+          stats(end, G) = str2num(value);
         case 'Evaluations'
-          evaluations(end + 1) = str2num(value);
+          stats(end, E) = str2num(value);
         case 'Deadline misses'
           tokens = regexp(value, '([\d.]+)\s*(\(.*\))?', 'tokens');
-          deadline_misses(end + 1) = str2num(tokens{1}{1});
-        case 'Best lifetime'
-          tokens = regexp(value, '\(([^,]+), .*', 'tokens');
-          if isempty(tokens)
-            lifetime(end + 1) = str2num(value);
-          else
-            lifetime(end + 1) = str2num(tokens{1}{1});
-          end
+          stats(end, D) = str2num(tokens{1}{1});
+        case 'Temperature runaways'
+          tokens = regexp(value, '([\d.]+)\s*(\(.*\))?', 'tokens');
+          stats(end, R) = str2num(tokens{1}{1});
         case 'Improvement'
-          tokens = regexp(value, '([^%]+)%.*\s+(-?[^%]+)%.*', 'tokens');
-          improvement = [ ...
-            improvement; ...
-            str2num(tokens{1}{1}) str2num(tokens{1}{2}) ...
-          ];
+          tokens = regexp(value, '(.+) lifetime with (.+) energy', 'tokens');
+          stats(end, IL) = str2num(tokens{1}{1});
+          stats(end, IE) = str2num(tokens{1}{2});
         case 'Assessed improvement'
-          tokens = regexp(value, '([^%]+)%.*\s+(-?[^%]+)%.*', 'tokens');
-          assessed_improvement = [ ...
-            assessed_improvement; ...
-            str2num(tokens{1}{1}) str2num(tokens{1}{2}) ...
-          ];
+          tokens = regexp(value, '(.+) lifetime with (.+) energy', 'tokens');
+          stats(end, AL) = str2num(tokens{1}{1});
+          stats(end, AE) = str2num(tokens{1}{2});
         case 'Time elapsed'
           tokens = regexp(value, '\s*([\d.]+\s*)', 'tokens');
-          time(end + 1) = str2num(tokens{1}{1});
+          stats(end, T) = str2num(tokens{1}{1});
         end
       end
 
       line = fgetl(fid);
     end
 
+    [ dummy, I ] = sort(stats(:, IL), 1, 'descend');
+
+    Stats(end + 1, :) = stats(I(1), :);
+
     fclose(fid);
   end
 
-  count = length(generations);
-
-  if length(assessed_improvement) == 0
-    assessed_improvement = zeros(size(improvement));
-  end
-
-  improvement = 1 + improvement / 100;
-  assessed_improvement = 1 + assessed_improvement / 100;
+  count = size(Stats, 1);
 
   fprintf('%10s%15s%15s%15s%15s%15s%15s%15s%15s\n', ...
-    'No', 'Generations', 'Evaluations', 'Deadline', ...
-    'Lifetime', '+LT, x', '+LTa, x', '+E, x', 'Time, m');
+    'No', 'Generations', 'Evaluations', 'Deadline', 'Runaways', ...
+    '+LT, x', '+LTa, x', '+E, x', 'Time, s');
+
   for i = 1:count
-    fprintf('%10d%15d%15d%15d%15.2f%15.2f%15.2f%15.2e%15.2f\n', ...
-      i, generations(i), evaluations(i), deadline_misses(i), ...
-      lifetime(i), improvement(i, 1), assessed_improvement(i, 1), ...
-      improvement(i, 2), time(i));
+    fprintf('%10d%15d%15d%15d%15d%15.2f%15.2f%15.2f%15.4f\n', ...
+      i, Stats(i, G), Stats(i, E), Stats(i, D), Stats(i, R), ...
+      Stats(i, IL), Stats(i, AL), Stats(i, IE), Stats(i, T));
   end
 
   fprintf('\n');
 
-  fprintf('%10s%15d%15d%15d%15.2f%15.2f%15.2f%15.2e%15.2f\n', 'min', ...
-    min(generations), min(evaluations), min(deadline_misses), ...
-    min(lifetime), min(improvement(:, 1)), min(assessed_improvement(:, 1)), ...
-    min(improvement(:, 2)), min(time));
+  fprintf('%10s%15d%15d%15d%15d%15.2f%15.2f%15.2f%15.2f\n', 'min', ...
+    min(Stats(:, G)),  min(Stats(:, E)),  min(Stats(:, D)), ...
+    min(Stats(:, R)),  min(Stats(:, IL)), min(Stats(:, AL)), ...
+    min(Stats(:, IE)), min(Stats(:, T)));
 
-  fprintf('%10s%15d%15d%15d%15.2f%15.2f%15.2f%15.2e%15.2f\n', 'max', ...
-    max(generations), max(evaluations), max(deadline_misses), ...
-    max(lifetime), max(improvement(:, 1)), max(assessed_improvement(:, 1)), ...
-    max(improvement(:, 2)), max(time));
+  fprintf('%10s%15d%15d%15d%15d%15.2f%15.2f%15.2f%15.2f\n', 'max', ...
+    max(Stats(:, G)),  max(Stats(:, E)),  max(Stats(:, D)), ...
+    max(Stats(:, R)),  max(Stats(:, IL)), max(Stats(:, AL)), ...
+    max(Stats(:, IE)), max(Stats(:, T)));
 
-  fprintf('%10s%15d%15d%15d%15.2f%15.2f%15.2f%15.2e%15.2f\n', 'mean', ...
-    round(mean(generations)), round(mean(evaluations)), ...
-    round(mean(deadline_misses)), mean(lifetime), ...
-    mean(improvement(:, 1)), mean(assessed_improvement(:, 1)), ...
-    mean(improvement(:, 2)), mean(time));
+  fprintf('%10s%15.0f%15.0f%15.0f%15.0f%15.2f%15.2f%15.2f%15.2f\n', 'mean', ...
+    mean(Stats(:, G)),  mean(Stats(:, E)),  mean(Stats(:, D)), ...
+    mean(Stats(:, R)),  mean(Stats(:, IL)), mean(Stats(:, AL)), ...
+    mean(Stats(:, IE)), mean(Stats(:, T)));
 
   if count > 1
     figure;
-    Utils.drawProgress('Improvement, %', improvement(:, 1));
+    Utils.drawProgress('Improvement, %', Stats(:, IL));
   end
 end
